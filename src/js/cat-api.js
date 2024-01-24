c41c1d27d593f2056b0bbb75944ffcd821094a84
Serhii Rybak <serhiirybak@Serhiis-MacBook-Pro.local>
2024-01-24 21:08:29 +0100

    Added js doc

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -3,10 +3,19 @@ import axios from 'axios';
 axios.defaults.headers.common['x-api-key'] =
   'live_eedEBIhSa1Zz96n6kEJ4AN8kcMgPva3zDP62EgDebovjszetJvu8mLFCWJ8MUFs7';
 
+/**
+ * Fetches all cat breeds
+ * @returns {Promise<AxiosResponse<any>>}
+ */
 function fetchBreeds() {
   return axios.get('https://api.thecatapi.com/v1/breeds');
 }
 
+/**
+ * Fetches a cat by breed
+ * @param {string} breedId
+ * @returns {Promise<AxiosResponse<any>>}
+ */
 function fetchCatByBreed(breedId) {
   const query = new URLSearchParams({ breed_ids: breedId });
   return axios.get(`https://api.thecatapi.com/v1/images/search?${query}`);
